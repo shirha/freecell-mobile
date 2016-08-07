694af4e7d9c8bc90adf21fa7862f626b4e667f97
shirha <sh1cpan@gmail.com>
2016-08-06 19:11:25 -0700

    clean up js

--- js/solver.js
+++ js/solver.js
@@ -89,7 +89,6 @@ function solve (e){
     for (var r = 0; r < MAXROWS; r++) {
       tableau[c][r] = e.data[c][r].rank === 0 ? 0 : e.data[c][r].rank + e.data[c][r].suit * 16 + 64;
   } } 
-  tableau = test(); 
   console.log( m(tableau) );
   
   init(), cnt++;
